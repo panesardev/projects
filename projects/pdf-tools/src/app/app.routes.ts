74ef841419ac7140cad809fb2de5d3d4fdbca5a1
Sukhpreet Singh <panesar.pb08@gmail.com>
2026-04-29 12:10:59 -0400

    save

--- projects/pdf-tools/src/app/app.routes.ts
+++ projects/pdf-tools/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: IndexComponent,
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '/',
+  }
+];
---
+++ projects/pdf-tools/src/app/pages/index/index.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index',
+  imports: [],
+  templateUrl: './index.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class IndexComponent {
+
+}
